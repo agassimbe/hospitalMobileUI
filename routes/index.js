c402bcfc51a0201170ed1a7029a2356a717182ad
User <azizgassimbe@gmail.com>
2024-11-12 10:42:40 +0000

    avant supp tab

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Routes = () => {
+  return (
+    <View>
+      <Text>index page</Text>
+    </View>
+  )
+}
+
+export default Routes
